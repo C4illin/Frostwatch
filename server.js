14d825d783db7689d808f6123f6bfc5322ed4a16
Emrik Östling <emrik.ostling@gmail.com>
2026-02-15 12:35:18 +0100

    remove dotenv

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const routeCache = require("route-cache");
 const axios = require("axios");
-require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
